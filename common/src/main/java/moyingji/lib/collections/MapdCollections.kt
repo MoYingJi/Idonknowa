1dd5b04dcb1ca6b00de25b0af7b01f0cb22e9c14
MoYingJi <moyingjiaw@outlook.com>
2024-11-13 12:41:28 +0800

    Rename args

--- common/src/main/java/moyingji/lib/collections/MapdCollections.kt
+++ common/src/main/java/moyingji/lib/collections/MapdCollections.kt
@@ -6,48 +6,48 @@ import moyingji.lib.util.*
 open class MapdCollection<T, R>(
     @Mutable open val parent: MutableCollection<T>,
     // pure function to convert element
-    val from: (T) -> R, val to: (R) -> T
+    val to: (T) -> R, val from: (R) -> T
 ) : MutableCollection<R> {
-    open fun from(element: T): R = from.invoke(element)
-    open fun to(element: R): T = to.invoke(element)
+    open fun to(element: T): R = to.invoke(element)
+    open fun from(element: R): T = from.invoke(element)
 
     override val size: Int get() = parent.size
     override fun clear() { parent.clear() }
     override fun isEmpty(): Boolean = parent.isEmpty()
 
-    override fun contains(element: R): Boolean = parent.contains(to(element))
-    override fun add(element: R): Boolean = parent.add(to(element))
-    override fun remove(element: R): Boolean = parent.remove(to(element))
+    override fun contains(element: R): Boolean = parent.contains(from(element))
+    override fun add(element: R): Boolean = parent.add(from(element))
+    override fun remove(element: R): Boolean = parent.remove(from(element))
 
-    override fun containsAll(elements: Collection<R>): Boolean = parent.containsAll(elements.map(::to).toSet())
-    override fun retainAll(elements: Collection<R>): Boolean = parent.retainAll(elements.map(::to).toSet())
-    override fun removeAll(elements: Collection<R>): Boolean = parent.removeAll(elements.map(::to).toSet())
-    override fun addAll(elements: Collection<R>): Boolean = parent.addAll(elements.map(::to))
+    override fun containsAll(elements: Collection<R>): Boolean = parent.containsAll(elements.map(::from).toSet())
+    override fun retainAll(elements: Collection<R>): Boolean = parent.retainAll(elements.map(::from).toSet())
+    override fun removeAll(elements: Collection<R>): Boolean = parent.removeAll(elements.map(::from).toSet())
+    override fun addAll(elements: Collection<R>): Boolean = parent.addAll(elements.map(::from))
 
-    override fun iterator(): MutableIterator<R> = MapdIterator(parent.iterator(), ::from)
+    override fun iterator(): MutableIterator<R> = MapdIterator(parent.iterator(), ::to)
 }
 
 open class MapdIterator<T, R>(
     @Mutable val parent: MutableIterator<T>,
-    val from: (T) -> R
+    val to: (T) -> R
 ) : MutableIterator<R> {
-    open fun from(element: T): R = from.invoke(element)
+    open fun to(element: T): R = to.invoke(element)
     override fun hasNext(): Boolean = parent.hasNext()
-    override fun next(): R = parent.next().let(::from)
+    override fun next(): R = parent.next().let(::to)
     override fun remove() { parent.remove() }
 }
 
 open class MapdMutableList<T, R>(
     @Mutable override val parent: MutableList<T>,
-    from: (T) -> R, to: (R) -> T
-) : MapdCollection<T, R>(parent, from, to), MutableList<R> {
-    override fun add(index: Int, element: R) { parent.add(index, to(element)) }
-    override fun addAll(index: Int, elements: Collection<R>): Boolean = parent.addAll(index, elements.map(::to))
-    override fun get(index: Int): R = from(parent[index])
-    override fun removeAt(index: Int): R = parent.removeAt(index).let(::from)
-    override fun set(index: Int, element: R): R = parent.set(index, to(element)).let(::from)
-    override fun lastIndexOf(element: R): Int = parent.lastIndexOf(to(element))
-    override fun indexOf(element: R): Int = parent.indexOf(to(element))
+    to: (T) -> R, from: (R) -> T
+) : MapdCollection<T, R>(parent, to, from), MutableList<R> {
+    override fun add(index: Int, element: R) { parent.add(index, from(element)) }
+    override fun addAll(index: Int, elements: Collection<R>): Boolean = parent.addAll(index, elements.map(::from))
+    override fun get(index: Int): R = to(parent[index])
+    override fun removeAt(index: Int): R = parent.removeAt(index).let(::to)
+    override fun set(index: Int, element: R): R = parent.set(index, from(element)).let(::to)
+    override fun lastIndexOf(element: R): Int = parent.lastIndexOf(from(element))
+    override fun indexOf(element: R): Int = parent.indexOf(from(element))
     override fun subList(fromIndex: Int, toIndex: Int): MutableList<R> = MutableSubList(this, fromIndex, toIndex - fromIndex)
     override fun listIterator(): MutableListIterator<R> = listIterator(0)
     override fun listIterator(index: Int): MutableListIterator<R> = MutableListIteratorImpl(this, index)
@@ -58,36 +58,36 @@ interface MapdCachable<T, R> { @Mutable val objectCache: MutableMap<T, R> }
 
 open class MapdCachedCollection<T, R>(
     @Mutable parent: MutableCollection<T>,
-    from: (T) -> R, to: (R) -> T,
+    to: (T) -> R, from: (R) -> T,
     @Mutable override val objectCache: MutableMap<T, R> = mutableMapOf(),
-    /** 函数 [to] 较为复杂时可以反向搜索缓存 */
+    /** 函数 [from] 较为复杂时可以反向搜索缓存 */
     val toCached: Boolean = false
-) : MapdCollection<T, R>(parent, from, to), MapdCachable<T, R> {
-    override fun from(element: T): R = objectCache.getOrPut(element) { from.invoke(element) }
-    override fun to(element: R): T = ifOrNull (toCached) {
+) : MapdCollection<T, R>(parent, to, from), MapdCachable<T, R> {
+    override fun to(element: T): R = objectCache.getOrPut(element) { to.invoke(element) }
+    override fun from(element: R): T = ifOrNull (toCached) {
         objectCache.firstKeyOfOrNull(element) } ?: toNoCache(element)
-    fun toNoCache(element: R): T = to.invoke(element).also { objectCache += it to element }
-    override fun iterator(): MutableIterator<R> = MapdCachedIterator(parent.iterator(), objectCache, ::from)
+    fun toNoCache(element: R): T = from.invoke(element).also { objectCache += it to element }
+    override fun iterator(): MutableIterator<R> = MapdCachedIterator(parent.iterator(), objectCache, ::to)
 }
 
 class MapdCachedIterator<T, R>(
     @Mutable parent: MutableIterator<T>,
     @Mutable override val objectCache: MutableMap<T, R> = mutableMapOf(),
-    from: (T) -> R
-) : MapdIterator<T, R>(parent, from), MapdCachable<T, R> {
-    override fun from(element: T): R = objectCache.getOrPut(element) { from.invoke(element) }
+    to: (T) -> R
+) : MapdIterator<T, R>(parent, to), MapdCachable<T, R> {
+    override fun to(element: T): R = objectCache.getOrPut(element) { to.invoke(element) }
 }
 
 class MapdCachedMutableList<T, R>(
     @Mutable parent: MutableList<T>,
-    from: (T) -> R, to: (R) -> T,
+    to: (T) -> R, from: (R) -> T,
     @Mutable override val objectCache: MutableMap<T, R> = mutableMapOf(),
-    /** 函数 [to] 较为复杂时可以反向搜索缓存 */
+    /** 函数 [from] 较为复杂时可以反向搜索缓存 */
     val toCached: Boolean = false
-) : MapdMutableList<T, R>(parent, from, to), MapdCachable<T, R> {
-    override fun from(element: T): R = objectCache.getOrPut(element) { from.invoke(element) }
-    override fun to(element: R): T = ifOrNull (toCached) {
+) : MapdMutableList<T, R>(parent, to, from), MapdCachable<T, R> {
+    override fun to(element: T): R = objectCache.getOrPut(element) { to.invoke(element) }
+    override fun from(element: R): T = ifOrNull (toCached) {
         objectCache.firstKeyOfOrNull(element) } ?: toNoCache(element)
-    fun toNoCache(element: R): T = to.invoke(element).also { objectCache += it to element }
+    fun toNoCache(element: R): T = from.invoke(element).also { objectCache += it to element }
     override fun listIterator(index: Int): MutableListIterator<R> = MutableListIteratorImpl(this, index)
 }
--- common/src/main/java/moyingji/lib/util/CollectionUtil.kt
+++ common/src/main/java/moyingji/lib/util/CollectionUtil.kt
@@ -10,28 +10,27 @@ fun <T> Iterator<T>.take(count: Int): Iterator<T> = CounteredIterator(this, coun
 
 // region xmap (Mutable)
 @Mutable fun <T, R> MutableCollection<T>.map(
-    from: (T) -> R, to: (R) -> T, cache: Boolean = false, toCache: Boolean = false
+    to: (T) -> R, from: (R) -> T, cache: Boolean = false, toCache: Boolean = false
 ): MutableCollection<R> { require(cache || !toCache)
-    return if (!cache) MapdCollection(this, from, to)
-    else MapdCachedCollection(this, from, to, toCached = toCache)
+    return if (!cache) MapdCollection(this, to, from)
+    else MapdCachedCollection(this, to, from, toCached = toCache)
 }
 @Mutable fun <T, R> MutableList<T>.map(
-    from: (T) -> R, to: (R) -> T, cache: Boolean = false, toCache: Boolean = false
+    to: (T) -> R, from: (R) -> T, cache: Boolean = false, toCache: Boolean = false
 ): MutableList<R> { require(cache || !toCache)
-    if (!cache) return MapdMutableList(this, from, to)
-    return MapdCachedMutableList(this, from, to, toCached = toCache)
+    if (!cache) return MapdMutableList(this, to, from)
+    return MapdCachedMutableList(this, to, from, toCached = toCache)
 }
-// 与普通的单向 kt std map 函数区分
-@Mutable fun <T, R> MutableCollection<T>.xmap(
-    cache: Boolean = false, toCache: Boolean = false, from: (T) -> R
-): ExpectTo<(R) -> T, MutableCollection<R>> {
+@Mutable fun <T, R> MutableCollection<T>.mapTo(
+    cache: Boolean = false, toCache: Boolean = false, to: (T) -> R
+): ExpectFrom<(R) -> T, MutableCollection<R>> {
     require(cache || !toCache)
-    return ExpectTo { to -> map(from, to, cache, toCache) }
+    return ExpectFrom { from -> map(to, from, cache, toCache) }
 }
-@Mutable fun <T, R> MutableList<T>.xmap(
-    cache: Boolean = false, toCache: Boolean = false, from: (T) -> R
-): ExpectTo<(R) -> T, MutableList<R>> {
+@Mutable fun <T, R> MutableList<T>.mapTo(
+    cache: Boolean = false, toCache: Boolean = false, to: (T) -> R
+): ExpectFrom<(R) -> T, MutableList<R>> {
     require(cache || !toCache)
-    return ExpectTo { to -> map(from, to, cache, toCache) }
+    return ExpectFrom { from -> map(to, from, cache, toCache) }
 }
 // endregion
